75ca4d6b86f6f573e41cfb06aac4b234fdb8105e
Steve Wake <steve@moveforward.co.nz>
2017-08-23 11:25:52 +1200

    fix rename issue

--- code/extensions/Model.php
+++ code/extensions/Model.php
@@ -4,14 +4,16 @@ namespace Modular\Extensions\Model;
 use Modular\Interfaces\Mappable as MappableInterface;
 use Modular\ModelExtension;
 use Modular\Traits\bitfield;
-use Modular\Traits\mappable_map_map;
 use Modular\Traits\mappable_mapper;
 use Modular\Traits\mappable_model;
 
-
-class Mappable extends ModelExtension implements MappableInterface {
+/**
+ * Mappable base extension, concrete instances need to also provide mappable_map_dots type functionality
+ *
+ * @package Modular\Extensions\Model
+ */
+abstract class Mappable extends ModelExtension implements MappableInterface {
 	use mappable_model;
-	use mappable_map_map;
 	use mappable_mapper;
 	use bitfield;
 
